7a9e103bf3ee29457d38eb239aea80dfb7719bb8
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-04-22 15:36:54 +0530

    firebase emulator setup

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// firebase
 import { initializeApp } from "firebase/app";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getStorage, ref, connectStorageEmulator } from "firebase/storage";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCFj3LT5eKykepGw2udtDmYsmb5sBXlH9Y",
@@ -16,6 +21,24 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth();
+
+// Create a root reference
+export const storage = getStorage(app);
+
+if (process.env.NODE_ENV === "development") {
+  try {
+    connectFirestoreEmulator(db, "localhost", 8080);
+    connectAuthEmulator(auth, "http://localhost:9099");
+    connectStorageEmulator(storage, "localhost", 9199);
+    console.log("connectAuthEmulator");
+  } catch (error) {
+    console.error(error);
+    console.log((error as any).message);
+  }
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
