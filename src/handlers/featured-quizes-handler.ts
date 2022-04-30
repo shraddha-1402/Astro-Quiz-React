987861dab6e9e97b6f7c909b9c834212f69f5fd8
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-04-30 17:12:28 +0530

    added id field to quiz data

--- src/handlers/featured-quizes-handler.ts
+++ src/handlers/featured-quizes-handler.ts
@@ -6,7 +6,7 @@ const getFeaturedQuizes = async () => {
     const querySnapshot = await getDocs(collection(db, "FeaturedQuizes"));
     let featuredQuizes: DocumentData[] = [];
     querySnapshot.forEach((doc) => {
-      if (doc.exists()) featuredQuizes.push(doc.data());
+      if (doc.exists()) featuredQuizes.push({ data: doc.data(), id: doc.id });
     });
     return featuredQuizes;
   } catch (error) {
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,29 +25,31 @@ const HomePage = () => {
       </p>
 
       <div className="flex-row justify-center gap-2 flex-wrap my-3-5">
-        {featuredQuizes.map(({ name, level, description, img }) => {
-          return (
-            <div className="card quiz-card mw-16r">
-              <div className="card-img-box">
-                <img
-                  src={img}
-                  alt="music-albums"
-                  className="card-img responsive-img quiz-card-img"
-                />
+        {featuredQuizes.map(
+          ({ data: { name, level, description, img }, id }) => {
+            return (
+              <div className="card quiz-card mw-16r" key={id}>
+                <div className="card-img-box">
+                  <img
+                    src={img}
+                    alt="music-albums"
+                    className="card-img responsive-img quiz-card-img"
+                  />
+                </div>
+                <div className="flex-col gap-0-25 m-0-5">
+                  <h3> {name} </h3>
+                  <p className="sm-text">{level}</p>
+                  <p className="sm-text">{description}</p>
+                  <a href="/Pages/rules.html" className="link">
+                    <button className="btn btn-solid-primary mt-1 mb-0-5 w-100p quiz-card-btn">
+                      Play Now
+                    </button>
+                  </a>
+                </div>
               </div>
-              <div className="flex-col gap-0-25 m-0-5">
-                <h3> {name} </h3>
-                <p className="sm-text">{level}</p>
-                <p className="sm-text">{description}</p>
-                <a href="/Pages/rules.html" className="link">
-                  <button className="btn btn-solid-primary mt-1 mb-0-5 w-100p quiz-card-btn">
-                    Play Now
-                  </button>
-                </a>
-              </div>
-            </div>
-          );
-        })}
+            );
+          }
+        )}
       </div>
     </div>
   );
