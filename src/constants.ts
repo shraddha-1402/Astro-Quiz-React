d06e26d786677f87279d74828ec796be78d778e7
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-05-17 18:35:20 +0530

    feat: default data state value

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,27 @@
+const dataStateValue = {
+  currentQuiz: {
+    name: "",
+    questions: [
+      {
+        question: "",
+        image: "",
+        options: [],
+        answerIndex: -1,
+      },
+    ],
+  },
+  userSelectedAnswers: [],
+  isQuizSelected: false,
+  currentQuestion: -1,
+};
+
 enum LocalRoutes {
   HOME = "/",
   LOGIN_PAGE = "/login-page",
   SIGNUP_PAGE = "/signup-page",
   FORGOT_PASSWORD_PAGE = "/forgot-password-page",
   PROFILE_PAGE = "/profile-page",
+  QUIZ_PAGE = "/quiz-page",
 }
 
 enum TestCredentials {
@@ -14,6 +32,12 @@ enum TestCredentials {
 enum ActionType {
   USER_LOGIN = "USER_LOGIN",
   USER_LOGOUT = "USER_LOGOUT",
+  SET_CURRENT_QUIZ = "SET_CURRENT_QUIZ",
+  SET_CURRENT_QUESTION = "SET_CURRENT_QUESTION",
+  SET_USER_SELECTED_ANSWERS = "SET_USER_SELECTED_ANSWERS",
+  SET_IS_QUIZ_SELECTED = "SET_IS_QUIZ_SELECTED",
+  RESET_DATASTATE = "RESET_DATASTATE",
 }
 
 export { LocalRoutes, TestCredentials, ActionType };
+export { dataStateValue };
