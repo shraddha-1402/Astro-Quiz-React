44e5aea2bf1ef5d799d71ddf344b743bc0d94933
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-05-17 19:23:06 +0530

    fix: added loader and try catch block

--- src/handlers/data.ts
+++ src/handlers/data.ts
@@ -1,45 +1,58 @@
 import { Dispatch } from "react";
+import { toast } from "react-toastify";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { DataAction } from "../types";
 import { ActionType } from "../constants";
+import { isFBError } from "../types/typeguards";
 
 const getQuizHandler = async ({
   quizId,
   dataDispatch,
+  setIsLoading,
 }: {
   quizId: string;
   dataDispatch: Dispatch<DataAction>;
+  setIsLoading: Dispatch<React.SetStateAction<boolean>>;
 }) => {
-  const response = await getDoc(doc(db, `quizes/${quizId}`));
-  if (response.exists()) {
-    const fbData = response.data() as {
-      name: string;
-      questions: {
-        answerIndex: number;
-        image: string;
-        options: string[];
-        question: string;
-      }[];
-    };
+  try {
+    setIsLoading(true);
+    const response = await getDoc(doc(db, `quizes/${quizId}`));
 
-    const data = {
-      name: fbData.name || "",
-      questions: fbData.questions || [],
-    };
+    if (response.exists()) {
+      const fbData = response.data() as {
+        name: string;
+        questions: {
+          answerIndex: number;
+          image: string;
+          options: string[];
+          question: string;
+        }[];
+      };
 
-    dataDispatch({
-      type: ActionType.SET_CURRENT_QUIZ,
-      payload: { currentQuiz: { ...data } },
-    });
-    dataDispatch({
-      type: ActionType.SET_CURRENT_QUESTION,
-      payload: { currentQuestion: 0 },
-    });
-    dataDispatch({
-      type: ActionType.SET_IS_QUIZ_SELECTED,
-      payload: { isQuizSelected: true },
-    });
+      const data = {
+        name: fbData.name || "",
+        questions: fbData.questions || [],
+      };
+
+      dataDispatch({
+        type: ActionType.SET_CURRENT_QUIZ,
+        payload: { currentQuiz: { ...data } },
+      });
+      dataDispatch({
+        type: ActionType.SET_CURRENT_QUESTION,
+        payload: { currentQuestion: 0 },
+      });
+      dataDispatch({
+        type: ActionType.SET_IS_QUIZ_SELECTED,
+        payload: { isQuizSelected: true },
+      });
+    }
+  } catch (error) {
+    if (isFBError(error))
+      toast.error(`${error.code.split("/")[1].split("-").join(" ")}`);
+  } finally {
+    setIsLoading(false);
   }
 };
 
