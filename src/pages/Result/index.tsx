0459af66bdaf9e98a689f6036d99f18b655c658e
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-05-19 18:29:41 +0530

    feat: reult page completed

--- src/pages/Result/index.tsx
+++ src/pages/Result/index.tsx
@@ -1,19 +1,108 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { LocalRoutes } from "../../constants";
-import { useDynamicTitle } from "../../hooks";
 import { useData } from "../../contexts";
+import { useDynamicTitle } from "../../hooks";
+import "./style.css";
 
 const ResultsPage = () => {
-  useDynamicTitle();
   const navigate = useNavigate();
+  useDynamicTitle();
+
   const {
-    dataState: { isQuizSelected },
+    dataState: {
+      userSelectedAnswers,
+      isQuizSelected,
+      currentQuiz: { questions },
+    },
   } = useData();
+
+  const getClass = (
+    index: number,
+    answerIndex: number,
+    selectedAnswer: number
+  ) => {
+    if (index === answerIndex)
+      return "option-label pointer-event-none checked-right";
+    else if (index === selectedAnswer)
+      return "option-label pointer-event-none checked-wrong";
+    else return "option-label pointer-event-none";
+  };
+
+  const [result, setResult] = useState({
+    score: 0,
+    message: "",
+  });
+
+  useEffect(() => {
+    let score = 0;
+    questions.forEach((question, index) => {
+      if (question.answerIndex === userSelectedAnswers[index]) score += 10;
+    });
+    if (result.score > 20)
+      setResult({ score, message: "Congratulations! You won the quiz 🎉🎉" });
+    else setResult({ score, message: "Oops! Better luck next time!" });
+  }, []);
+
   return (
     <div>
       {isQuizSelected ? (
         <>
-          <h1 className="center-text">You Result will be displayed here</h1>
+          <h1 className="center-text my-2">Quiz Result</h1>
+          <p className="lg-text center-text"> {result.message} </p>
+          <p className="lg-text text-bold-weight center-text m-0-5">
+            Score: {result.score} / 50
+          </p>
+          <h2 className="center-text my-3-5">Check Answers</h2>
+          {questions.map(({ image, question, options, answerIndex }, idx) => {
+            return (
+              <div className="my-2" key={idx}>
+                <div className="flex-row mx-auto justify-spc-bet mw-48r">
+                  <span className="md-text mb-0-5">Question: {idx + 1}/5</span>
+                  <span className="md-text mb-0-5 red-text">
+                    {userSelectedAnswers[idx] === -1 && "No option selected"}
+                  </span>
+                </div>
+                <div className="grid-2-col gap-2 container mx-auto  mw-48r">
+                  <div className="flex-col justify-center">
+                    <img
+                      src={image}
+                      alt="question1-img"
+                      className="responsive-img"
+                    />
+                  </div>
+                  <div className="flex-col">
+                    <p>{question}</p>
+                    <div className="flex-col justify-center">
+                      {options.map((option, index) => {
+                        return (
+                          <div key={index} className="w-100p">
+                            <input
+                              className="option-input"
+                              type="radio"
+                              name="option"
+                              id={`option${index}`}
+                            />
+                            <label
+                              className={getClass(
+                                index,
+                                answerIndex,
+                                userSelectedAnswers[idx]
+                              )}
+                              htmlFor={`option${index}`}
+                            >
+                              {option}
+                            </label>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+
           <div className="flex-row justify-center mx-auto my-3-5">
             <button
               onClick={() => navigate(LocalRoutes.HOME, { replace: true })}
